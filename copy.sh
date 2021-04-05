3569c2f7921c789cb2ea1af6dc270f75f97b2de1
koyu <me@koyu.space>
2021-04-05 12:12:45 +0200

    Add rofi configs

--- copy.sh
+++ copy.sh
@@ -2,7 +2,9 @@ cp ~/.bashrc .
 mkdir -p .config/yay
 mkdir -p .config/polybar
 mkdir -p .config/i3
+mkdir -p .config/rofi
 cp ~/.config/yay/config.json .config/yay
 cp ~/.config/polybar/config .config/polybar
 cp ~/.config/i3/config .config/i3
+cp -ra ~/.config/rofi/. .config/rofi
 cp ~/.bg.jpg .
