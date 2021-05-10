1e50d6238081de05b0c2616076c067e283747ef2
koyu <me@koyu.space>
2021-05-10 18:35:41 +0200

    Fix spotify-tui

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ cp .bashrc ~
 cp .bg.png ~
 cp -r .config ~
 sudo cp i3lock-koyu /usr/bin/i3lock-koyu
+sudo cp spotify.sh /usr/bin/spotify-client
 sudo chmod +x /usr/bin/i3lock-koyu
 sudo cp wakelock.service /lib/systemd/system/wakelock.service
 sudo systemctl enable wakelock
---
+++ spotify.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+spotifyd
+spt
+killall spotifyd
