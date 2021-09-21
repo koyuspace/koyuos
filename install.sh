fc1715d2eda9b584a968aef70cb7add9165b955a
koyu <me@koyu.space>
2021-09-21 20:39:30 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,11 @@ sudo cp i3lock-koyu /usr/bin/i3lock-koyu
 sudo cp spotify.sh /usr/bin/spotify-client
 sudo chmod +x /usr/bin/i3lock-koyu
 sudo cp wakelock.service /lib/systemd/system/wakelock.service
+sudo mkdir -p /usr/src/koyuos/
+sudo git clone https://github.com/koyuspace/koyuos /usr/src/koyuos/
+sudo cp issue /etc/issue
+sudo cp upgrade.sh /usr/bin/koyuos-upgrade
+sudo chmod +x /usr/bin/koyuos-upgrade
 sudo systemctl enable wakelock
 sudo systemctl enable ly
 sudo systemctl enable bluetooth
