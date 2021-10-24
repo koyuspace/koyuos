ddc1e33b34128ab64f6cb0f2a27a3fca6f6a0189
koyu <me@koyu.space>
2021-10-25 01:45:02 +0200

    Order is important

--- upgrade.sh
+++ upgrade.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+cd /usr/src/koyuos
 cp .config/rofi/themes/koyu.rasi ~/.config/rofi/themes/koyu.rasi
 sudo cp issue /etc/issue
-cd /usr/src/koyuos
 sudo git pull
 sudo cp upgrade.sh /usr/bin/koyuos-upgrade
 yay --noconfirm --needed
