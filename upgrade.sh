7be57c1f9cac62ee84909668916c9d11f8057574
koyu <me@koyu.space>
2021-12-26 01:59:46 +0100

    Remove rofi line

--- upgrade.sh
+++ upgrade.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cd /usr/src/koyuos
-cp .config/rofi/themes/koyu.rasi ~/.config/rofi/themes/koyu.rasi
 sudo cp issue /etc/issue
 sudo git pull
 sudo cp upgrade.sh /usr/bin/koyuos-upgrade
