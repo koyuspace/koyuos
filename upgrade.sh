27578b65d235e9808dbec6897d6d8abeac908461
koyu <me@koyu.space>
2022-02-16 18:30:01 +0000

    Add lsb-release to upgrade script

--- upgrade.sh
+++ upgrade.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /usr/src/koyuos
 sudo cp issue /etc/issue
+sudo cp lsb-release /etc/lsb-release
 sudo git pull
 sudo cp upgrade.sh /usr/bin/koyuos-upgrade
 yay -Syu --noconfirm --needed
